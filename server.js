758c37640bdaf62ff6dc846567dec3668e89e529
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-06-08 02:12:11 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var http = require('http');
+var Docker = require('dockerode');
+var docker = new Docker();
+
 
 console.log('here');
 //create a server object:
 var portnumber = process.env.PORT || 8080;
 http.createServer(function (req, res) {
 	console.log('here');
-  res.write('Hello World!'); //write a response to the client
+	docker.createContainer({Image: 'sandbox', Cmd: ['python', 'progpy.py'], name: 'ubuntu-test'}, function (err, container) {
+	  container.start(function (err, data) {
+	    //...
+	    console.log(data);
+	  });
+	});
+  res.write(data); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 8080 
