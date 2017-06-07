2d1e0c1caffeb3d4412e5ece4b3dd65f0f7ce553
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-06-08 00:43:26 +0530

    initial commit

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
-from flask import Flask
+import docker
+from flask import Flask, request
+from flask_restful import Resource, Api
+import os
+
+client = docker.from_env()
+print('in here! fellas!!')
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+stdout = {}
 
-if __name__ == "__main__":
-    app.run()
+
+class TodoSimple(Resource):
+    def get(self):
+    	container = client.containers.run("sandbox","python progpy.py",detach=True)
+        return {stdout: "hello world"}
+
+    # def put(self, todo_id):
+    #     todos[todo_id] = request.form['data']
+    #     return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/')
+
+port = int(os.environ.get("PORT", 80))
+app.run(host='0.0.0.0', port=port)
