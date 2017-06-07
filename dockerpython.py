a71cbff752a24a3234eff5791c125d7a2aa260cc
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-06-07 18:29:19 +0530

    initial commit

--- dockerpython.py
+++ dockerpython.py
@@ -1,5 +1,5 @@
 import docker
 client = docker.from_env()
 print('in here! fellas!!')
-client.containers.run("sandbox", "python progpy.py")
+client.containers.run("sandbox", "echo helloworld")
 client.containers.run("sandbox", "python progpy.py")
