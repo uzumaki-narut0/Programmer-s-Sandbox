06e378c6fe2eb25e45d9995cd273af34c75791d8
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-06-08 16:11:49 +0530

    minor changes in server code

--- server.py
+++ server.py
@@ -27,7 +27,27 @@ class TodoSimple(Resource):
     	print(stdout)
         return {"stdout": stdout}
 
-api.add_resource(TodoSimple, '/<string:code>')
+    def post(self):
+    	parser = reqparse.RequestParser()
+        parser.add_argument('langid');
+        parser.add_argument('code')
+        #parser.add_argument('lng')
+        args = parser.parse_args()
+        language = args['langid']
+        code = args['code']
+        #lng = args['lng']
+        with open('progpy.py', 'w') as f:
+    		f.write(code)
+    	#making file executable 
+    	client.images.build(path='.',tag='sandbox')
+    	print(os.getcwd())
+    	st = os.stat('output.py')
+    	os.chmod('output.py', st.st_mode | stat.S_IEXEC)
+    	stdout = client.containers.run("sandbox",["python","progpy.py"])
+    	print(stdout)
+        return {"stdout": stdout}  
+
+api.add_resource(TodoSimple, '/')
 
 port = int(os.environ.get("PORT", 82))
 app.run(host='localhost', port=port)
